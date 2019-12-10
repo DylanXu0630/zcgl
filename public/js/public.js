91920b5eb8d984a4617698ddccb5ea7f65e29c08
DlyanXu <365997572@qq.com>
2019-12-10 13:39:02 +0800

    修改

--- public/js/public.js
+++ public/js/public.js
@@ -5,10 +5,10 @@ var login = "http://192.168.44.78:18001/auth"
 // var IPdz = "http://61.160.81.178:21003/uum"
 // var IPzd = "http://61.160.81.178:21002/estate"
 
-// var IPdz = "http://192.168.10.56:18000/uum"
-// var IPzd = "http://172.1.1.151:14000/estate"
 var IPdz = "http://192.168.10.56:18000/uum"
-var IPzd = "http://192.168.10.56:14000/estate"
+var IPzd = "http://172.1.1.151:14000/estate"
+// var IPdz = "http://192.168.10.56:18000/uum"
+// var IPzd = "http://192.168.10.56:14000/estate"
 // var IPzd = "http://192.168.10.56:14000/estate"
 var indexDig, lookDig
 /*当前登录的用户ID*/
