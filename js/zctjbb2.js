2b44b6b718195549638162ef0665db5a3cf68e2a
DlyanXu <365997572@qq.com>
2019-12-18 14:07:18 +0800

    Merge branch 'dev-twp' into release
    # Conflicts:
    #	js/role.js

--- js/zctjbb2.js
+++ js/zctjbb2.js
@@ -3,22 +3,6 @@ layui.use('element', function () {
     var element = layui.element;
 });
 
-Date.prototype.Format = function (fmt) {
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "H+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-}
-
 layui.use('table', function () {
     var table = layui.table;
 
@@ -30,8 +14,8 @@ layui.use('table', function () {
         async: false,//请求是否异步，默认为异步，这也是ajax重要特性
         type: "POST",   //请求方式
         data: JSON.stringify({
-            "date" : new Date().Format("yyyy-MM-dd")
-        }), 
+            "date": sjc(getNOW() + "1日 23:59:59")
+        }),
         contentType: "application/json;charset=UTF-8",
         // headers: {"token": sessionStorage.token},
         beforeSend: function () {
@@ -54,12 +38,12 @@ layui.use('table', function () {
         , data: dataList
         , page: false //开启分页
         , cols: [[ //表头
-            {type: 'numbers', title: '序号', fixed: 'left',rowspan:2}
-            , {field: 'agency', title: '资产管理单位', sort: true, width: 100,rowspan:2}
-            , {align: 'center', title: '本月新增出租面积', sort: true, width: 200,colspan:4}
-            , {align: 'center', title: '本月减少出租面积', sort: true, width: 200,colspan:4}
-            , {align: 'center', title: '房产出售、办证情况（累计）', sort: true, width: 200,colspan:2}
-        ],[
+            {type: 'numbers', title: '序号', fixed: 'left', rowspan: 2}
+            , {field: 'agency', title: '资产管理单位', sort: true, width: 100, rowspan: 2}
+            , {align: 'center', title: '本月新增出租面积', sort: true, width: 200, colspan: 4}
+            , {align: 'center', title: '本月减少出租面积', sort: true, width: 200, colspan: 4}
+            , {align: 'center', title: '房产出售、办证情况（累计）', sort: true, width: 200, colspan: 2}
+        ], [
             {field: 'oldRentArea', title: '其中：续租', sort: true, width: 200}
             , {field: 'newRentArea', title: '新增出租', sort: true, width: 200}
             , {field: 'totalMonthRentArea', title: '小计', sort: true, width: 200}
@@ -78,13 +62,21 @@ layui.use('table', function () {
         var laydate = layui.laydate;
         getgldwcheck()
         form.render()
-        lay('.httime').each(function () {
-            laydate.render({
-                elem: this,
-                format: 'yyyy年MM月dd日'
-            });
-        })
-    
+        laydate.render({
+            elem: '.httime',
+            format: 'yyyy年MM月',
+            value: getNOW(),
+            type: 'month',
+            ready: function (date) {
+                $("#layui-laydate1").off('click').on('click', '.laydate-month-list li', function () {
+                    $("#layui-laydate1").remove();
+                });
+            },
+            change: function (value, dates, edate) {
+                $('.httime').val(value);
+            }
+        });
+
         $("#sousuo").on("click", function () {
             form.on('submit(search)', function (data) {
                 // getDatas()
@@ -95,8 +87,8 @@ layui.use('table', function () {
                     async: false,//请求是否异步，默认为异步，这也是ajax重要特性
                     type: "POST",   //请求方式
                     data: JSON.stringify({
-                        "date" : sjc($('#s-date').val())
-                    }), 
+                        "date": sjc($('#s-date').val() + "1日 23:59:59")
+                    }),
                     contentType: "application/json;charset=UTF-8",
                     // headers: {"token": sessionStorage.token},
                     beforeSend: function () {
@@ -112,12 +104,12 @@ layui.use('table', function () {
                         //请求出错处理
                     }
                 });
-    
+
                 table.reload('tableList', {
                     data: dataList
                 });
                 return false;//false：阻止表单跳转  true：表单跳转
-            }) 
+            })
         })
         $("#sx").on("click", function () {
             window.reload
@@ -126,115 +118,10 @@ layui.use('table', function () {
 })
 
 
-
-
 // $(function () {
 //     getDatas()
 // })
 
-function getDatas() {
-    var agency = []
-    if ($(".mangit").length !== 0) {
-        agency = []
-        $(".mangit").each(function (i, o) {
-            if ($(this).is(':checked')) {
-                agency.push($(o).attr("id"))
-            }
-        })
-    } else {
-        agency = []
-    }
-
-    if ($("#s-date2").val() !== "") {
-        var endStart = $("#s-date2").val()
-    } else {
-        var endStart = getYear() + "年12月31日"
-    }
-
-    if ($("#s-date").val() !== "") {
-        var startDate = $("#s-date").val()
-    } else {
-        var startDate = getYear() + "年01月01日"
-    }
-
-
-    var data = {
-        "agency": agency,
-        "endStart": sjc(endStart),
-        "startDate": sjc(startDate)
-    }
-
-    $.ajax({
-        url: IPzd + '/report/house_resource',    //请求的url地址
-        dataType: "json",   //返回格式为json
-        async: false,//请求是否异步，默认为异步，这也是ajax重要特性
-        type: "POST",   //请求方式
-        data: JSON.stringify(data),
-        contentType: "application/json;charset=UTF-8",
-        // headers: {"token": sessionStorage.token},
-        beforeSend: function () {
-            //请求前的处理
-        },
-        success: function (req) {
-            if (req.status == 200) {
-
-            }
-            //     $(".month").children().remove()
-            //     $(".totalResourceArea").children().remove()
-            //     $(".totalResourceYzArea").children().remove()
-            //     $(".totalResourceWzArea").children().remove()
-            //     $(".totalRentedResourceArea").children().remove()
-            //     $(".totalResourceNum").children().remove()
-            //     $(".totalRentedResourceNum").children().remove()
-            //
-            //     var zgldw = $("<td class='bj'>月份</td>").appendTo(".month")
-            //     var qfTd = $("<td class='bj'>房源总面积(m²)</td>").appendTo(".totalResourceArea")
-            //     var zqtmj = $("<td class='bj'>总有证面积(m²)</td>").appendTo(".totalResourceYzArea")
-            //     var zqtmj = $("<td class='bj'>总无证面积(m²)</td>").appendTo(".totalResourceWzArea")
-            //     var zfynum = $("<td class='bj'>总出租面积(m²)</td>").appendTo(".totalRentedResourceArea")
-            //     var zqtmj = $("<td class='bj'>房源总数</td>").appendTo(".totalResourceNum")
-            //     var zjzmj = $("<td class='bj'>总出租个数</td>").appendTo(".totalRentedResourceNum")
-            //
-            //     var mtd = $("<td class='mouthTD' colspan='" + req.data.length + "'>" + req.data[0].totalResourceArea + "</td>").appendTo(".totalResourceArea")
-            //     var ytd = $("<td class='mouthTD' colspan='" + req.data.length + "'>" + req.data[0].totalResourceYzArea + "</td>").appendTo(".totalResourceYzArea")
-            //     var std = $("<td class='mouthTD' colspan='" + req.data.length + "'>" + req.data[0].totalResourceWzArea + "</td>").appendTo(".totalResourceWzArea")
-            //     var qtd = $("<td class='mouthTD' colspan='" + req.data.length + "' >" + req.data[0].totalResourceNum + "</td>").appendTo(".totalResourceNum")
-            //
-            //     // $(req.data).each(function (i, o) {
-            //     //     var mtd = $("<td class='mouthTD bjTD'>" + o.month + "</td>").appendTo(".month")
-            //     //
-            //     //     var qtd = $("<td class='mouthTD'>" + o.totalRentedResourceNum + "</td>").appendTo(".totalRentedResourceNum")
-            //     //     var qtd = $("<td class='mouthTD'>" + o.totalRentedResourceArea + "</td>").appendTo(".totalRentedResourceArea")
-            //     // })
-            // } else {
-            //     $(".month").children().remove()
-            //     $(".totalRentedResourceArea").children().remove()
-            //     $(".totalRentedResourceNum").children().remove()
-            //     $(".totalResourceArea").children().remove()
-            //     $(".totalResourceNum").children().remove()
-            //     $(".totalResourceWzArea").children().remove()
-            //     $(".totalResourceYzArea").children().remove()
-            //
-            //
-            //     var zgldw = $("<td class='bj'>月份</td>").appendTo(".month")
-            //     var qfTd = $("<td class='bj'>房源总面积(m²)</td>").appendTo(".totalResourceArea")
-            //     var zqtmj = $("<td class='bj'>总有证面积(m²)</td>").appendTo(".totalResourceYzArea")
-            //     var zqtmj = $("<td class='bj'>总无证面积(m²)</td>").appendTo(".totalResourceWzArea")
-            //     var zfynum = $("<td class='bj'>总出租面积</td>").appendTo(".totalRentedResourceArea")
-            //     var zqtmj = $("<td class='bj'>房源总数</td>").appendTo(".totalResourceNum")
-            //     var zjzmj = $("<td class='bj'>总出租个数</td>").appendTo(".totalRentedResourceNum")
-            //
-            //     var td = $("<td style='text-align: center' rowspan='7'>暂无数据</td>").appendTo(".month")
-            // }
-        },
-        complete: function () {
-            //请求完成的处理
-        },
-        error: function () {
-            //请求出错处理
-        }
-    });
-}
 
 function getgldwcheck() {
     $.ajax({
@@ -268,8 +155,14 @@ function getgldwcheck() {
     });
 }
 
-function getYear() {
-    var date = new Date;
-    var y = date.getFullYear()
-    return y
+
+function getNOW() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    var currentdate = year + "年" + month + "月";
+    return currentdate;
 }
--- js/zctjbb3.js
+++ js/zctjbb3.js
@@ -2,9 +2,25 @@
 layui.use('element', function () {
     var element = layui.element;
 });
-layui.use('table', function () {
+layui.use(['table', 'laydate', 'form'], function () {
     var table = layui.table;
+    var laydate = layui.laydate;
+    var form = layui.form
 
+    laydate.render({
+        elem: '.httime',
+        format: 'yyyy年MM月',
+        value: getNOW(),
+        type: 'month',
+        ready: function (date) {
+            $("#layui-laydate1").off('click').on('click', '.laydate-month-list li', function () {
+                $("#layui-laydate1").remove();
+            });
+        },
+        change: function (value, dates, edate) {
+            $('.httime').val(value);
+        }
+    });
     var dataList = []
 
     $.ajax({
@@ -13,8 +29,8 @@ layui.use('table', function () {
         async: false,//请求是否异步，默认为异步，这也是ajax重要特性
         type: "POST",   //请求方式
         data: JSON.stringify({
-            "date" : "2019-12-17"
-        }), 
+            "date": sjc(getNOW() + "1日 23:59:59")
+        }),
         contentType: "application/json;charset=UTF-8",
         // headers: {"token": sessionStorage.token},
         beforeSend: function () {
@@ -38,26 +54,76 @@ layui.use('table', function () {
         , data: dataList
         , page: false //开启分页
         , cols: [[ //表头
-            {type: 'numbers', title: '序号', fixed: 'left',rowspan:3}
-            , {field: 'agency', title: '资产管理单位', sort: true, width: 200,rowspan:3}
-            , {align: 'center', title: '租金收入情况', sort: true, width: 200,colspan:5}
-            , {align: 'center', title: '销售收入情况', sort: true, width: 200,colspan:2}
-            , {align: 'center', title: '收到往年欠租', sort: true, width: 200,colspan:2}
-            , {align: 'center', title: '备注', sort: true, width: 200,rowspan:3}
-            , {align: 'center', title: '考核目标', sort: true, width: 200,rowspan:3}
-        ],[
-            {align: 'center', title: '应收租金', sort: true, width: 200,colspan:2}
-            , {align: 'center', title: '到账租金', sort: true, width: 200,colspan:2}
-            , {field: 'rate', title: '到账率', sort: true, width: 200,rowspan:2}
-            , {align: 'center', title: '本年累计应收', sort: true, width: 200,rowspan:2}
-            , {align: 'center', title: '本年累计到账', sort: true, width: 200,rowspan:2}
-            , {field: 'monthArrears', title: '本月', sort: true, width: 200,rowspan:2}
-            , {field: 'yearArrears', title: '本年累计', sort: true, width: 200,rowspan:2}
-        ],[
+            {type: 'numbers', title: '序号', fixed: 'left', rowspan: 3}
+            , {field: 'agency', title: '资产管理单位', sort: true, width: 200, rowspan: 3}
+            , {align: 'center', title: '租金收入情况', sort: true, width: 200, colspan: 5}
+            , {align: 'center', title: '销售收入情况', sort: true, width: 200, colspan: 2}
+            , {align: 'center', title: '收到往年欠租', sort: true, width: 200, colspan: 2}
+            , {align: 'center', title: '备注', sort: true, width: 200, rowspan: 3}
+            , {align: 'center', title: '考核目标', sort: true, width: 200, rowspan: 3}
+        ], [
+            {align: 'center', title: '应收租金', sort: true, width: 200, colspan: 2}
+            , {align: 'center', title: '到账租金', sort: true, width: 200, colspan: 2}
+            , {field: 'rate', title: '到账率', sort: true, width: 200, rowspan: 2}
+            , {align: 'center', title: '本年累计应收', sort: true, width: 200, rowspan: 2}
+            , {align: 'center', title: '本年累计到账', sort: true, width: 200, rowspan: 2}
+            , {field: 'monthArrears', title: '本月', sort: true, width: 200, rowspan: 2}
+            , {field: 'yearArrears', title: '本年累计', sort: true, width: 200, rowspan: 2}
+        ], [
             {field: 'monthMustMoney', title: '本月', sort: true, width: 200}
-            ,{field: 'yearMustMoney', title: '本年累计', sort: true, width: 200}
-            ,{field: 'monthRealMoney', title: '本月', sort: true, width: 200}
-            ,{field: 'yearRealMoney', title: '本年累计', sort: true, width: 200}
+            , {field: 'yearMustMoney', title: '本年累计', sort: true, width: 200}
+            , {field: 'monthRealMoney', title: '本月', sort: true, width: 200}
+            , {field: 'yearRealMoney', title: '本年累计', sort: true, width: 200}
         ]]
     });
+
+
+    $("#sousuo").on("click", function () {
+        form.on('submit(search)', function (data) {
+            // getDatas()
+            //执行重载
+            $.ajax({
+                url: IPzd + '/report/rent/money',    //请求的url地址
+                dataType: "json",   //返回格式为json
+                async: false,//请求是否异步，默认为异步，这也是ajax重要特性
+                type: "POST",   //请求方式
+                data: JSON.stringify({
+                    "date": sjc($('#s-date').val() + "1日 23:59:59")
+                }),
+                contentType: "application/json;charset=UTF-8",
+                // headers: {"token": sessionStorage.token},
+                beforeSend: function () {
+                    //请求前的处理
+                },
+                success: function (req) {
+                    dataList = req.data
+                },
+                complete: function () {
+                    //请求完成的处理
+                },
+                error: function () {
+                    //请求出错处理
+                }
+            });
+
+            table.reload('tableList', {
+                data: dataList
+            });
+            return false;//false：阻止表单跳转  true：表单跳转
+        })
+    })
+    $("#sx").on("click", function () {
+        window.reload
+    })
 })
+
+function getNOW() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    var currentdate = year + "年" + month + "月";
+    return currentdate;
+}
