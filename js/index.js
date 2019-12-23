fc8d477b86b1abd3e1e4aea25eefd8de53daf3e6
DlyanXu <365997572@qq.com>
2019-12-23 09:45:11 +0800

    Merge branch 'dev-twp' into release
    # Conflicts:
    #	js/role.js

--- js/index.js
+++ js/index.js
@@ -175,7 +175,7 @@ $(function () {
 // 	titCell:"dl", //鼠标触发对象
 // 	targetCell:"ul", //与titCell一一对应，第n个titCell控制第n个targetCell的显示隐藏
 // 	effect:"slideDown", //targetCell下拉效果
-// 	delayTime:300 , //效果时间
+// 	delayTime:300 , //效果时间zzzzzzzzzz
 // 	triggerTime:150, //鼠标延迟触发时间（默认150）
 // 	defaultPlay:true,//默认是否执行效果（默认true）
 // 	trigger:"click",
