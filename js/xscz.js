0f7a5101ee0df76f19bfa71dfbe36a29b418c1ca
DlyanXu <365997572@qq.com>
2020-01-06 14:56:24 +0800

    Merge branch 'dev-twp' into release
    # Conflicts:
    #	js/role.js

--- js/xscz.js
+++ js/xscz.js
@@ -1880,27 +1880,7 @@ layui.use(['laydate', 'table', 'form'], function () {
                 }
                 layerOpen(openMes);
             } else if (layEvent == 'dc') {
-                $.ajax({
-                    url: IPzd + '/deal/word/' + obj.data.id,    //请求的url地址
-                    dataType: "json",   //返回格式为json
-                    async: false,//请求是否异步，默认为异步，这也是ajax重要特性
-                    type: "GET",   //请求方式
-                    contentType: "application/json;charset=UTF-8",
-                    // headers: {"token": sessionStorage.token},
-                    beforeSend: function () {
-                        //请求前的处理
-                    },
-                    success: function (req) {
-
-                    },
-
-                    complete: function () {
-                        //请求完成的处理
-                    },
-                    error: function () {
-                        //请求出错处理
-                    }
-                });
+                window.location.href = IPzd + '/deal/word/' + obj.data.id
             }
         }
     );
